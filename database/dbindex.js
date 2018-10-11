07bc6eb6773e104299e1069aef75148c4e2d2216
Ryan Heise <heis007ryan@gmail.com>
2018-10-11 12:37:18 -0500

    adds partial use for interval updating of database interval not set up yet

--- database/dbindex.js
+++ database/dbindex.js
@@ -18,16 +18,45 @@ const User = stackDb.define('User', {
   comment: { type: Sequelize.ARRAY(Sequelize.TEXT) },
 });
 
+// FOR FUTURE IF WANT TO ADD UPVOTES
 // const Comments = stackDb.define('Comments', {
 // comment: { type: Sequelize.ARRAY(Sequelize.TEXT) },
 // upVotes: {type: Sequelize.INTEGER}
 // });
 
-stackDb.sync()
-  .then(() => {
-    User.create({})
-      .then(() => {});
-  });
+//stackDb.sync();
+
+
+const updateAnswers = (answersObj) => {
+  const { username, answers } = answersObj;
+  return User.find({ where: { userName: username } })
+    .then((data) => {
+      if (data !== null) {
+        return User.update({
+          comment: answers,
+        });
+      }
+      throw new Error('User does not exist');
+    });
+};
+// updateAnswers({username: 'theRobinKim', answers: ['hello', 'bad', 'good'] })
+
+
+const findAllIds = () => {
+  return User.findAll({ attributes: ['userId'] }).then((data) => {
+    const arrayOfIds = [];
+    data.forEach((id) => {
+      arrayOfIds.push(id.dataValues.userId);
+    });
+    return arrayOfIds;
+  })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
 
 module.exports.User = User;
+module.exports.updateAnswers = updateAnswers;
+module.exports.findAllIds = findAllIds;
 // module.exports.Comments = Comments;
